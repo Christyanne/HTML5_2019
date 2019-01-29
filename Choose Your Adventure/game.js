2bcdaff8c216f58e7c28d800fc372bffd3b1fcb9
Christy <christy.c051@stu.nebo.edu>
2019-01-29 10:24:52 -0700

    Fixing problems in game and can now end game other unknown problems but still works

--- Choose Your Adventure/game.js
+++ Choose Your Adventure/game.js
@@ -15,29 +15,28 @@ Multiline comment
 
 Game();
 
-function Game(){
+function Game() {
     document.write("Legends of Oria");
     var playerName = prompt("What is your name?");
-    alert("Welcome to the land of Oira "+ playerName);
+    alert("Welcome to the land of Oria " + playerName);
     
     MainRoom();
     
-    function MainRoom(){
-        var MainRoom = prompt("You wake up... Your head is pounding and your vision blurred. After pacing around walking off the headache your visions clears. It appears you are in an empty room and you can't remember why. /n - look around /n - go back to sleep n/-examine room").toLowerCase();
+    function MainRoom() {
+        var MainRoom = prompt("You wake up... Your head is pounding and your vision blurred. After pacing around walking off the headache your visions clears. It appears you are in an empty room and you can't remember why. /n - look around /n - go back to sleep").toLowerCase();
         
-        if (MainRoom == "look around" || ER == "look"){
-            var ERLook = prompt ("The room is small, dusty, drips of some substance seep into the room from the celing. There are two doors. One leading North and one leading South. The North door is locked but the rest seem open to explore. And on the floor is the only thing in the cementroom. A rug. It's visably moldy and smells terable. /n - open south door /n - move rug")
+        if (MainRoom === "look around" || MainRoom === "look") {
+            var MainRoomLook = prompt("The room is small, dusty, drips of some substance seep into the room from the celing. There are two doors. One leading North and one leading South. The North door is locked but the rest seem open to explore. And on the floor is the only thing in the cementroom. A rug. It's visably moldy and smells terrible. /n - open south door /n - move rug");
+        }  
+        else if (MainRoom === "go back to sleep" || MainRoom === "sleep") {
+            alert("You fall back to sleep on the moldy rug. You dream of distant lands and adventures of a distant but not entierly forgotten past.");
+            var resume = confirm ("Do you wish to wake up?");
             
-        }
-        else if(MainRoom == "go back to sleep" || prison == "sleep"){
-            alert("You fall back to sleep on the moldy rug. You dream of distant lands and adventures");
-            var resume = confirm ("Do you wish to continue?");
-            
-            if (resume){
+            if (resume) {
                 MainRoom();
             }
             else{
-                alert ("Game Over! You Failed!");
+                alert ("Game Over! You Failed! You Slept Into A Coma!");
             
             }
         }
