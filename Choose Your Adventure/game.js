5fabe146454f5ead757df0a79b7dde9872a4acf1
Christy <christy.c051@stu.nebo.edu>
2019-02-28 10:29:09 -0700

    fixing problems and failing and the pie hit her in the face

--- Choose Your Adventure/game.js
+++ Choose Your Adventure/game.js
@@ -19,7 +19,7 @@ var player = {
         coins: {
             pink: 0,
             purple: 0,
-            blue: 1,
+            blue: 1
         },
         books: {
             diary: 0,
@@ -38,7 +38,7 @@ var player = {
             pillow: 0
         }
     }
-}
+};
 function GetRandInt(max) {
     var randInt = Math.floor(Math.random() * Math.floor(max));
     return randInt;
@@ -46,7 +46,7 @@ function GetRandInt(max) {
     var checkCoins = function (pink, purple, blue) {
         alert("Pink Coins:" + player.inventory.coins.pink + "/ -n Purple Coins: ");
         (+player.inventory.coins.purple + "/ -n Blue Coins: " + player.inventory.coins.blue);
-    }
+    };
 //Start Game
     Game();
 
@@ -54,8 +54,8 @@ function GetRandInt(max) {
         var legendNames = ["Oria", "Iridia", "Cartan", "Rixen"];
         document.write("Legends of" + legendNames[Math.floor(Math.random() * 4)] + "!");
         var playerName = prompt("What is your name?");
-        while(!confirm("Are you sure you want "+ playerName+" as a name?")){
-            playerName = prompt ("What name do you want?");
+        while (!confirm("Are you sure you want " + playerName + " as a name?")) {
+            playerName = prompt("What name do you want?");
         }
         alert("Welcome to the land of Drust " + playerName);
     
@@ -66,7 +66,7 @@ function GetRandInt(max) {
 
             if (MainRoom === "look around" || MainRoom === "look") {
                 var MainRoomLook = prompt("The room is small, dusty, drips of some substance seep into the room from the celing. There are two doors. One leading North and one leading South. The North door is locked but the rest seem open to explore. And on the floor is the only thing in the cementroom. A rug. It's visably moldy and smells terrible. /n - open south door /n - move rug");
-            }  
+            }   
                 else if (MainRoom === "go back to sleep" || MainRoom === "sleep") {
                 alert("You fall back to sleep on the moldy rug. You dream of distant lands and adventures of a distant but not entierly forgotten past.");
                 var resume = confirm("Do you wish to wake up? ");
@@ -78,9 +78,9 @@ function GetRandInt(max) {
                         alert("Game Over! You Failed! You Slept Into A Coma! AKA a dedicated sleep. ");
 
                     }
-            else if(MainRoom == CrytalCave){
-                CrystalCave();
-            }
+                if (MainRoom === CrystalCave) {
+                    CrystalCave();
+                }
             }
 
             function CrystalCave() {
@@ -88,28 +88,29 @@ function GetRandInt(max) {
 
 
                 if (crystalcaveEnv === "swim in pool" || "swim") {
-                    var crystalWaters = prompt("You swim around the pool. And it strangly makes no ripples in the water. The crystals reflect off the surface of the water but you don't see you own reflection. It feels refreshing. You wonder when the last time you had a bath was. /n - get out");
+                    var CrystalWaters = prompt("You swim around the pool. And it strangly makes no ripples in the water. The crystals reflect off the surface of the water but you don't see you own reflection. It feels refreshing. You wonder when the last time you had a bath was. /n - get out");
                     if (crystalcaveEnv === "get out") {
                         alert("You get out and your are dripping wet because you didn't take off your close.");
                     }
                 }
                     else if (crystalcaveEnv === "throw rock" || "throw") {
                     alert("You throw rock into pool but it doesn't wripple. As soon as its done hopping is sinks to an unknown bottom.");
-                }   
-                if (crystalcaveEnv === "look around" || "look") {
-                    var crystalcaveEnv = prompt("You scan the room and see writing on the wall. It spells out Keepers of the Orians. A strange icky sensaton creeps up your spine like someones watching you. You glance around but see nothing there.");
-                }
-                    else {
-                        alert("I don't understand" + crystalcaveEnv);
-                        CrystalCave();
+                   
+                    if (crystalcaveEnv === "look around" || "look") {
+                        var crystalcaveEnv = prompt("You scan the room and see writing on the wall. It spells out Keepers of the Orians. A strange icky sensaton creeps up your spine like someones watching you. You glance around but see nothing there.");
                     }
-                switch (new Area().getArea()) {
-                case MainRoom:
-                    Area = "MainRoom";
-                    break;
-                case CrystalCave:
-                    Area = "CrystalCave";
-                    break;
+                        else {
+                            alert("I don't understand" + crystalcaveEnv);
+                            CrystalCave();
+                        }
+                    switch (new Location().getlocation()) {
+                    case MainRoom:
+                        location = "MainRoom";
+                        break;
+                    case CrystalCave:
+                        location = "CrystalCave";
+                        break;
+                    }
                 }
             }
         }
